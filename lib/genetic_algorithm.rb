965c95c2de520dd3730c605a251fe6f6dcead45c
James Tunnell <jamestunnell@gmail.com>
2014-05-04 14:57:56 -0700

    Add initial files.

--- lib/genetic_algorithm.rb
+++ lib/genetic_algorithm.rb
@@ -1,1 +1,6 @@
 require 'genetic_algorithm/version'
+
+require 'genetic_algorithm/individual'
+require 'genetic_algorithm/algorithms/simple_ga'
+require 'genetic_algorithm/selectors/tournament_selector'
+require 'genetic_algorithm/encodings/vector_encoding'
