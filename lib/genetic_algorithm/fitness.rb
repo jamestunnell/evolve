c9803d320b488456be2b802bab3ca82f7d02e01b
James Tunnell <jamestunnell@gmail.com>
2014-05-04 16:08:31 -0700

    Fix something.

--- lib/genetic_algorithm/fitness.rb
+++ lib/genetic_algorithm/fitness.rb
@@ -1,26 +1,33 @@
 module GeneticAlgorithm
   class Fitness
-    include Comparable
-    
     attr_reader :value
-    def initialize value
-      @value = value
-    end
   end
   
   class FitnessTowardZero < Fitness
+    def initialize value
+      @value = value
+    end
+    
     def <=> other
       @value.abs < other.value.abs
     end
   end
   
   class FitnessTowardInfinity < Fitness
+    def initialize value
+      @value = value
+    end
+    
     def <=> other
       @value > other.value
     end
   end
   
   class FitnessTowardNegativeInfinity < Fitness
+    def initialize value
+      @value = value
+    end
+    
     def <=> other
       @value < other.value
     end
