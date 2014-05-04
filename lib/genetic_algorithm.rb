d495373b62479bab11b32ea280c9242a75693cde
James Tunnell <jamestunnell@gmail.com>
2014-05-04 15:04:41 -0700

    Add Fitness class.

--- lib/genetic_algorithm.rb
+++ lib/genetic_algorithm.rb
@@ -1,6 +1,7 @@
 require 'genetic_algorithm/version'
 
 require 'genetic_algorithm/individual'
+require 'genetic_algorithm/fitness'
 require 'genetic_algorithm/algorithms/simple_ga'
 require 'genetic_algorithm/selectors/tournament_selector'
 require 'genetic_algorithm/encodings/vector_encoding'
