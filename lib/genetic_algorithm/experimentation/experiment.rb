17e6a637c1586e61b19c69b1cc7ded8ecf20b02f
James Tunnell <jamestunnell@gmail.com>
2014-06-05 14:47:00 -0700

    Add control for verbosity of experiment progress updates. Set the progress_updates variable to one of the constants in Experiment::PROGRESS_UPDATES (currently, :none, :minimal, and :verbose).

--- lib/genetic_algorithm/experimentation/experiment.rb
+++ lib/genetic_algorithm/experimentation/experiment.rb
@@ -1,21 +1,29 @@
 module GeneticAlgorithm
   class Experiment
+    NONE = :none
+    MINIMAL = :minimal
+    VERBOSE = :verbose
+    PROGRESS_UPDATES = [ NONE, MINIMAL, VERBOSE]
+    
+    attr_accessor :progress_updates
     def initialize algorithm
       @algorithm = algorithm
+      @progress_updates = NONE
     end
     
     def self.avg_fitness population
       population.map {|x| x.fitness }.average
     end
     
-    def run pop_size, seed_fn, stopping_fn, print_progress: false
+    def run pop_size, seed_fn, stopping_fn
       population = Array.new(pop_size) {|i| seed_fn.call() }
       population.sort!
       best_so_far = population.last
       avg_fitnesses = { 0 => Experiment.avg_fitness(population) }
       best_fitnesses = { 0 => best_so_far.fitness }
   
-      if print_progress
+      case @progress_updates
+      when VERBOSE
         puts "gen\tavg\best\tbestsofar"
       end
       
@@ -33,10 +41,19 @@ module GeneticAlgorithm
           best_fitnesses[n] = best.fitness
         end
         
-        if print_progress
+        case @progress_updates
+        when VERBOSE
           puts "#{n}\t#{avg_fitness}\t#{best.fitness}\t#{best_so_far.fitness}"
+        when MINIMAL
+          print "."
         end
       end
+      
+      case @progress_updates
+      when VERBOSE, MINIMAL
+        puts
+      end
+      
       return Run.new(best_so_far, best_fitnesses, avg_fitnesses)
     end
   end
